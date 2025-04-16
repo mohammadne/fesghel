04af61fca0fedc6457bc8c67e35d084549700624
mohammadne <mohammadne.dev@gmail.com>
2025-04-16 10:36:56 +0330

    chore: add languages

---
+++ internal/api/http/i18n/i18n.go
@@ -0,0 +1,104 @@
+package i18n
+
+import (
+	"embed"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"go.uber.org/zap"
+
+	"github.com/mohammadne/fesghel/internal/entities"
+)
+
+//go:embed languages/*.json
+var languages embed.FS
+
+type I18N interface {
+	Translate(key string, language entities.Language) string
+}
+
+type i18n struct {
+	logger   *zap.Logger
+	messages map[entities.Language]map[string]any
+}
+
+func New(logger *zap.Logger) (I18N, error) {
+	i := &i18n{
+		logger:   logger,
+		messages: make(map[entities.Language]map[string]any),
+	}
+
+	files, err := languages.ReadDir("languages")
+	if err != nil {
+		return nil, fmt.Errorf("error reading languages directory, %v", err)
+	}
+
+	for _, file := range files {
+		name := file.Name()
+		if len(name) < 6 || name[len(name)-5:] != ".json" {
+			continue
+		}
+
+		languageRaw := name[:len(name)-5]
+		language := entities.ToLanguage(languageRaw)
+		if languageRaw != string(language) {
+			return nil, fmt.Errorf("invalid file language %s, %v", name, err)
+		}
+
+		data, err := languages.ReadFile("languages/" + name)
+		if err != nil {
+			return nil, fmt.Errorf("error reading language file %s, %v", name, err)
+		}
+
+		var messages map[string]any
+		if err := json.Unmarshal(data, &messages); err != nil {
+			return nil, fmt.Errorf("error parsing language file %s, %v", name, err)
+		}
+
+		i.messages[language] = messages
+	}
+
+	return i, nil
+}
+
+func (i *i18n) Translate(key string, language entities.Language) string {
+	if translations, exists := i.messages[language]; exists {
+		keyParts := strings.Split(key, ".")
+		var current any = translations
+
+		for index := 0; index <= len(keyParts); index++ {
+			lastElement := index == len(keyParts)
+
+			switch current1 := current.(type) {
+			case string:
+				if !lastElement {
+					break
+				}
+				return current1
+
+			case map[string]any:
+				if lastElement {
+					break
+				}
+
+				if translation, found := current1[keyParts[index]]; found {
+					current = translation
+					continue
+				} else {
+					break
+				}
+			}
+		}
+
+		i.logger.Error("key not found", zap.String("key", key))
+		return translateFromKey(key)
+	}
+
+	i.logger.Error("local not found", zap.String("locale", string(language)))
+	return translateFromKey(key)
+}
+
+func translateFromKey(key string) string {
+	return strings.ReplaceAll(key, ".", " ")
+}
---
+++ internal/api/http/i18n/i18n_test.go
@@ -0,0 +1,52 @@
+package i18n_test
+
+import (
+	"testing"
+
+	"go.uber.org/zap"
+
+	"github.com/mohammadne/fesghel/internal/api/http/i18n"
+	"github.com/mohammadne/fesghel/internal/entities"
+)
+
+func TestReader(t *testing.T) {
+	i18n, err := i18n.New(zap.NewNop())
+	if err != nil {
+		t.Errorf("error while creating i18n %v", err)
+	}
+
+	tests := []struct {
+		description string
+		language    entities.Language
+		key         string
+		expected    string
+	}{
+		{
+			description: "testing non exsisting locale",
+			language:    "fr",
+			key:         "non.existing.locale",
+			expected:    "non existing locale",
+		},
+		{
+			description: "testing non exsisting key in a valid locale",
+			language:    entities.LanguageEnglish,
+			key:         "non.existing.key",
+			expected:    "non existing key",
+		},
+		{
+			description: "testing an exsisting key in a valid locale",
+			language:    entities.LanguageEnglish,
+			key:         "categories.list.success",
+			expected:    "Categories have been retrieved successfully",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			translation := i18n.Translate(tt.key, tt.language)
+			if translation != tt.expected {
+				t.Errorf("Translate(%q, %q) = %q; want %q", tt.key, tt.language, translation, tt.expected)
+			}
+		})
+	}
+}
