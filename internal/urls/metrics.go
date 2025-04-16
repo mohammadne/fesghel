e131b959fd4a7c10223c834138c5c7d89eac9560
mohammadne <mohammadne.dev@gmail.com>
2025-04-16 11:36:07 +0330

    chore: update urls metrics

--- internal/urls/metrics.go
+++ internal/urls/metrics.go
@@ -12,22 +12,23 @@ type metrics struct {
 	Histogram metrics_pkg.Histogram
 }
 
-func newMetrics() (metrics *metrics, err error) {
+func newMetrics() (m *metrics, err error) {
+	m = &metrics{}
 	var prefix = "urls"
 
 	counterName := prefix + "_counter"
 	counterLabels := []string{"method", "status"}
-	metrics.Counter, err = metrics_pkg.RegisterCounter(counterName, entities.Namespace, entities.System, counterLabels)
+	m.Counter, err = metrics_pkg.RegisterCounter(counterName, entities.Namespace, entities.System, counterLabels)
 	if err != nil {
 		return nil, fmt.Errorf("error while registering counter vector: %v", err)
 	}
 
 	histogramName := prefix + "_histogram"
 	histogramLabels := []string{"method"}
-	metrics.Histogram, err = metrics_pkg.RegisterHistogram(histogramName, entities.Namespace, entities.System, histogramLabels)
+	m.Histogram, err = metrics_pkg.RegisterHistogram(histogramName, entities.Namespace, entities.System, histogramLabels)
 	if err != nil {
 		return nil, fmt.Errorf("error while registering histogram vector: %v", err)
 	}
 
-	return metrics, nil
+	return m, nil
 }
